356f84de0308a61932e55d622c2150432e9ae829
mcamatta <camattamichel@gmail.com>
2023-07-25 22:16:13 -0300

    refactor: utilizando service para listagem de produtos

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,11 +1,20 @@
-import { IProduto, produtos } from './../produtos';
-import { Component } from '@angular/core';
+import { ProdutosService } from './../produtos.service';
+import { IProduto } from './../produtos';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent {
-  produtos: IProduto[] = produtos;
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto[] | undefined;
+
+  constructor(
+    private ProdutosService: ProdutosService
+  ) {}
+
+  ngOnInit(): void {
+    this.produtos = this.ProdutosService.getAll();
+  }
 }
