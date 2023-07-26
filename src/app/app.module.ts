0efa43708da31ffea3b660c43dcd6a379a1ff886
mcamatta <camattamichel@gmail.com>
2023-07-25 21:48:43 -0300

    feat: Adicionado componente para páginas não existentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
