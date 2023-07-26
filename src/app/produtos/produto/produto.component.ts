911bd46eeea5af73137866ed8181833a4afa138b
mcamatta <camattamichel@gmail.com>
2023-07-25 22:16:47 -0300

    feat: Adicionando dinamicidade no produto

--- src/app/produtos/produto/produto.component.ts
+++ src/app/produtos/produto/produto.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from './../../produtos.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.component.html',
   styleUrls: ['./produto.component.css']
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
+
+  produto: IProduto | undefined;
+
+  constructor(
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get('id'));
+    this.produto = this.produtosService.getOne(produtoId);
+    console.log(this.produto);
+    
+  }
 
 }
